14527f88364dba41eda4620a67e8c38952e2a6b2
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-11-24 17:37:21 +0100

    on ajoute les differents types de reponses

--- src/main/java/coo/questionnaire/YesNoAnswer.java
+++ src/main/java/coo/questionnaire/YesNoAnswer.java
@@ -1,18 +1,42 @@
 package coo.questionnaire;
 
+import coo.questionnaire.gui.AnswerPanel;
+import coo.questionnaire.gui.YesNoAnswerPanel;
+
 public class YesNoAnswer extends Answer<YesNo> {
 
-    public YesNoAnswer(YesNo valueOf) {
-        super(valueOf);
-    }
-
-    @Override
-    public boolean accept(String var1) {
-        return var1.equals(YesNo.values());
-    }
-
-    @Override
     public String instruction() {
-        return "oui ou non";
+        String instruction = "";
+        YesNo[] var5;
+        int var4 = (var5 = YesNo.values()).length;
+
+        for(int var3 = 0; var3 < var4; ++var3) {
+            YesNo value = var5[var3];
+            instruction = instruction + " " + value.name();
+        }
+
+        return instruction;
+    }
+
+    @Override
+    public AnswerPanel createMyAnswerPanel() {
+        return new YesNoAnswerPanel(this);
+    }
+
+    public YesNoAnswer(YesNo theGoodAnswer) {
+        super(theGoodAnswer);
+    }
+
+    public YesNoAnswer(String answerText) {
+        this(YesNo.valueOf(answerText));
+    }
+
+    public boolean accept(String s) {
+        try {
+            YesNo.valueOf(s);
+            return true;
+        } catch (IllegalArgumentException var3) {
+            return false;
+        }
     }
 }
