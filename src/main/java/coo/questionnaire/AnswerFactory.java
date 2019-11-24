b6d4f1343537de211527ac72f7c8348365134867
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-11-24 17:35:28 +0100

    ajout des questions à reponses multiples

--- src/main/java/coo/questionnaire/AnswerFactory.java
+++ src/main/java/coo/questionnaire/AnswerFactory.java
@@ -1,5 +1,10 @@
 package coo.questionnaire;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class AnswerFactory {
 
     public static final AnswerFactory FACTORY = new AnswerFactory();
@@ -9,9 +14,36 @@ public class AnswerFactory {
             return new NumericalAnswer(new Integer(answer));
         } else if (new YesNoAnswer(YesNo.values()[0]).accept(answer)) {
             return new YesNoAnswer(YesNo.valueOf(answer));
+        } else if (answer.indexOf(59) != -1) {
+            return this.buildMultiAnswer(answer);
+        } else {
+            return (Answer)(answer.indexOf(124) != -1 ? this.buildMultipleChoice(answer) : new TextAnswer(answer));
+        }
+    }
+
+    private MultipleChoiceAnswer buildMultipleChoice(String answer) {
+        LinkedList<String> choices = new LinkedList();
+        Scanner sc = (new Scanner(answer)).useDelimiter(Pattern.compile("\\|"));
+        String rightAnswer = sc.next().trim();
+
+        while(sc.hasNext()) {
+            choices.add(sc.next().trim());
         }
 
-        return null;
+        sc.close();
+        return new MultipleChoiceAnswer(rightAnswer, choices);
+    }
+
+    private MultiAnswer buildMultiAnswer(String answer) {
+        List<String> answers = new LinkedList();
+        Scanner sc = (new Scanner(answer)).useDelimiter("\\s;\\s");
+
+        while(sc.hasNext()) {
+            answers.add(sc.next().trim());
+        }
+
+        sc.close();
+        return new MultiAnswer(answers);
     }
 
 }
